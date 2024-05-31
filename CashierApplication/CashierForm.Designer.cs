7a54b20f58e0f908f85c35a824497222eb1c498a
STI-ADMIN <STI-ADMIN@LAB-A-PC24>
2024-05-31 10:31:00 +0800

    Add project files.

---
+++ CashierApplication/CashierForm.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace CashierApplication
+{
+    partial class CashierForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.paymentR = new System.Windows.Forms.Label();
+            this.changess = new System.Windows.Forms.Label();
+            this.TotalAmount = new System.Windows.Forms.Label();
+            this.changes = new System.Windows.Forms.Label();
+            this.ItemName = new System.Windows.Forms.TextBox();
+            this.discounts = new System.Windows.Forms.TextBox();
+            this.prices = new System.Windows.Forms.TextBox();
+            this.quantityy = new System.Windows.Forms.TextBox();
+            this.PRecieved = new System.Windows.Forms.TextBox();
+            this.Compute = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(301, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Discount (%):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(301, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Amount:";
+            // 
+            // paymentR
+            // 
+            this.paymentR.AutoSize = true;
+            this.paymentR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paymentR.Location = new System.Drawing.Point(22, 306);
+            this.paymentR.Name = "paymentR";
+            this.paymentR.Size = new System.Drawing.Size(145, 20);
+            this.paymentR.TabIndex = 5;
+            this.paymentR.Text = "Payment Recieved:";
+            // 
+            // changess
+            // 
+            this.changess.AutoSize = true;
+            this.changess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changess.Location = new System.Drawing.Point(98, 360);
+            this.changess.Name = "changess";
+            this.changess.Size = new System.Drawing.Size(69, 20);
+            this.changess.TabIndex = 6;
+            this.changess.Text = "Change:";
+            // 
+            // TotalAmount
+            // 
+            this.TotalAmount.AutoSize = true;
+            this.TotalAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalAmount.Location = new System.Drawing.Point(210, 252);
+            this.TotalAmount.Name = "TotalAmount";
+            this.TotalAmount.Size = new System.Drawing.Size(49, 20);
+            this.TotalAmount.TabIndex = 7;
+            this.TotalAmount.Text = "00.00";
+            // 
+            // changes
+            // 
+            this.changes.AutoSize = true;
+            this.changes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changes.Location = new System.Drawing.Point(210, 360);
+            this.changes.Name = "changes";
+            this.changes.Size = new System.Drawing.Size(49, 20);
+            this.changes.TabIndex = 8;
+            this.changes.Text = "00.00";
+            // 
+            // ItemName
+            // 
+            this.ItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItemName.Location = new System.Drawing.Point(17, 65);
+            this.ItemName.Name = "ItemName";
+            this.ItemName.Size = new System.Drawing.Size(208, 26);
+            this.ItemName.TabIndex = 9;
+            // 
+            // discounts
+            // 
+            this.discounts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discounts.Location = new System.Drawing.Point(305, 65);
+            this.discounts.Name = "discounts";
+            this.discounts.Size = new System.Drawing.Size(100, 26);
+            this.discounts.TabIndex = 10;
+            // 
+            // prices
+            // 
+            this.prices.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prices.Location = new System.Drawing.Point(63, 106);
+            this.prices.Name = "prices";
+            this.prices.Size = new System.Drawing.Size(162, 26);
+            this.prices.TabIndex = 11;
+            // 
+            // quantityy
+            // 
+            this.quantityy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityy.Location = new System.Drawing.Point(375, 112);
+            this.quantityy.Name = "quantityy";
+            this.quantityy.Size = new System.Drawing.Size(85, 26);
+            this.quantityy.TabIndex = 12;
+            // 
+            // PRecieved
+            // 
+            this.PRecieved.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PRecieved.Location = new System.Drawing.Point(201, 308);
+            this.PRecieved.Name = "PRecieved";
+            this.PRecieved.Size = new System.Drawing.Size(100, 26);
+            this.PRecieved.TabIndex = 13;
+            // 
+            // Compute
+            // 
+            this.Compute.BackColor = System.Drawing.Color.IndianRed;
+            this.Compute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Compute.Location = new System.Drawing.Point(214, 169);
+            this.Compute.Name = "Compute";
+            this.Compute.Size = new System.Drawing.Size(87, 31);
+            this.Compute.TabIndex = 14;
+            this.Compute.Text = "Compute";
+            this.Compute.UseVisualStyleBackColor = false;
+            this.Compute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(316, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(468, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitApplicationToolStripMenuItem,
+            this.logoutToolStripMenuItem1});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // exitApplicationToolStripMenuItem
+            // 
+            this.exitApplicationToolStripMenuItem.Name = "exitApplicationToolStripMenuItem";
+            this.exitApplicationToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitApplicationToolStripMenuItem.Text = "Exit Application";
+            this.exitApplicationToolStripMenuItem.Click += new System.EventHandler(this.exitApplicationToolStripMenuItem_Click);
+            // 
+            // logoutToolStripMenuItem1
+            // 
+            this.logoutToolStripMenuItem1.Name = "logoutToolStripMenuItem1";
+            this.logoutToolStripMenuItem1.Size = new System.Drawing.Size(157, 22);
+            this.logoutToolStripMenuItem1.Text = "Logout";
+            this.logoutToolStripMenuItem1.Click += new System.EventHandler(this.logoutToolStripMenuItem1_Click);
+            // 
+            // CashierForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(468, 399);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Compute);
+            this.Controls.Add(this.PRecieved);
+            this.Controls.Add(this.quantityy);
+            this.Controls.Add(this.prices);
+            this.Controls.Add(this.discounts);
+            this.Controls.Add(this.ItemName);
+            this.Controls.Add(this.changes);
+            this.Controls.Add(this.TotalAmount);
+            this.Controls.Add(this.changess);
+            this.Controls.Add(this.paymentR);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CashierForm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label paymentR;
+        private System.Windows.Forms.Label changess;
+        private System.Windows.Forms.Label TotalAmount;
+        private System.Windows.Forms.Label changes;
+        private System.Windows.Forms.TextBox ItemName;
+        private System.Windows.Forms.TextBox discounts;
+        private System.Windows.Forms.TextBox prices;
+        private System.Windows.Forms.TextBox quantityy;
+        private System.Windows.Forms.TextBox PRecieved;
+        private System.Windows.Forms.Button Compute;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitApplicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem1;
+    }
+}
+
---
+++ CashierApplication/CashierForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CashierApplication
+{
+    public partial class CashierForm : Form
+    {
+        public CashierForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                     
+            exitApplicationToolStripMenuItem.Enabled = true;
+        }
+
+        private void logoutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        DiscountedItem DI;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = ItemName.Text;
+            double price = double.Parse(prices.Text);
+            int quantity = Convert.ToInt32(quantityy.Text);
+            double discount = double.Parse(discounts.Text);
+            DI = new DiscountedItem (name, price, quantity, discount);
+            double totalPrice = DI.getTotalPrice();
+            TotalAmount.Text = totalPrice.ToString();
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            double AmountPayment = double.Parse(PRecieved.Text);
+            DI.setPayment(AmountPayment);
+            double change = DI.getChange();
+            changes.Text  =  change.ToString();
+        }
+    }
+}
